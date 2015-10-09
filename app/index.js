18ce4a0d71022eb52292ec6cf02a372c9cca95c7
Anna Lezhikova <anna.v.l@gmail.com>
2015-10-09 21:25:51 +1300

    Add console log to test observable

--- app/index.js
+++ app/index.js
@@ -23,15 +23,22 @@ function intent(DOM) {
 }
 
 function model(actions) {
+  const list = []
+  const list$ = Rx.Observable.from(list).subscribe
   const position$ = actions.map(ev => {
+      list.push({x: ev.pageX, y: ev.pageY})
+      list$.map(obj => console.log(obj))
       return {x: ev.pageX, y: ev.pageY}
+
   })
+
   return position$
 }
 
 function view(item$) {
   console.log('view')
   return item$.map(position => {
+    console.log("position", position)
     const style = {
       width: '50px',
       top: String(position.y+'px'),
