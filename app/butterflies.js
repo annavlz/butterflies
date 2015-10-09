5bced303ffac34195cfe61a25e19cf047887d846
Anna Lezhikova <anna.v.l@gmail.com>
2015-10-09 13:34:10 +1300

    commit

---
+++ app/butterflies.js
@@ -0,0 +1,5 @@
+/** @jsx hJSX */
+
+import {Rx} from '@cycle/core';
+import {hJSX} from '@cycle/dom';
+// import butterflies from './butterflies'
--- app/index.js
+++ app/index.js
@@ -1,29 +1,13 @@
 /** @jsx hJSX */
-import Cycle from '@cycle/core';
-import {makeDOMDriver, hJSX} from '@cycle/dom';
 
-function main(responses) {
-  let none = {
-    display: none
-  }
-  let block = {
-    display: block
-  }
-  let requests = {
-    DOM: responses.DOM.select('input').events('change')
-      .map(ev => ev.target.checked)
-      .startWith(false)
-      .map(toggled =>
-          <div>
-            <input type="checkbox" /> Toggle me
-            <p>{ toggled ? 'ON' : 'off' }</p>
-              { toggled ? <img src={'./assets/image.gif'} /> : '' }
-          </div>
-      )
-  };
-  return requests;
+import {run} from '@cycle/core';
+import {makeDOMDriver, hJSX} from '@cycle/dom';
+import butterflies from './butterflies'
+
+function main(sources) {
+  return butterflies(sources)
 }
 
-Cycle.run(main, {
+run(main, {
  DOM: makeDOMDriver('#main-container')
 });
